0be65cf17fe9ffa0975b4ff1c5b6d3260ce99a77
iluvatar1 <woquendo@gmail.com>
2025-05-21 12:50:11 -0500

    Added script to generate data

---
+++ 2025-05-21-opt-I/generate_data.sh
@@ -0,0 +1,10 @@
+
+# explorar como dependen los tiempos respecto al tamanho de la matriz, block size, y optimizacion
+for OPT in 0 1 2 3; do 
+    gcc -O$OPT transpose_blocking.c
+    MSIZE=512; 
+    for ((csize=1; csize <= $MSIZE; csize *= 2)); do 
+        echo -n $MSIZE $csize " "; 
+        ./a.out $MSIZE $csize; 
+    done > data-$OPT.txt
+done
